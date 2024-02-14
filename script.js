031deb490a4db5d932266453864d6368605d1274
Kenneth Espaldon <153700378+kennethespaldon@users.noreply.github.com>
2024-02-14 14:42:39 -0800

    create getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "use strict";
 
 function getRandomNumber() {
-    return Math.floor(Math.random() * 3);
+  return Math.floor(Math.random() * 3);
+}
+
+function getComputerChoice() {
+  let randomNum = getRandomNumber();
+
+  switch (randomNum) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
 }
