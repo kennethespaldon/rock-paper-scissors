58166901143af9edca1844f7e9f461714b3d7689
Kenneth Espaldon <153700378+kennethespaldon@users.noreply.github.com>
2024-02-14 16:52:02 -0800

    separate checkSolutions function into 2 boolean functions and create playRound function

--- script.js
+++ script.js
@@ -23,18 +23,38 @@ function capitalizeString(str) {
   return str.slice(0, 1).toUpperCase() + str.slice(1);
 }
 
-function checkSelections(playerSelection, computerSelection) {
-  if (playerSelection !== computerSelection) {
-    if (
-      (playerSelection === "Rock" && computerSelection === "Scissors") ||
-      (playerSelection === "Paper" && computerSelection === "Rock") ||
-      (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
+function isRoundTie(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return true;
+  }
+
+   return false;
+}
+
+function doesPlayerWinRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function outputRoundResults(playerSelection, computerSelection) {
+  if (isRoundTie(playerSelection, computerSelection)) {
+    if (doesPlayerWinRound(playerSelection, computerSelection)) {
       return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-      return `You lose! ${playerSelection} beats ${computerSelection}`;
+      return `You Lose! ${playerSelection} beats ${computerSelection}`;
     }
-  } else {
-    return "It's a tie!";
   }
+
+  return "It's a tie";
+}
+
+function playRound(playerSelection, computerSelection) {
+  return checkSelections(playerSelection, computerSelection);
 }
