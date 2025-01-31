5af42ec739aedfd393a62bc78355b60c8eff9f2a
Kenneth Espaldon <kenespaldon@gmail.com>
2025-01-31 13:48:05 -0800

    create function that returns computer choice

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function getRandomNumber()
 {
   return Math.floor(Math.random() * 3);
 }
+
+function getComputerChoice() 
+{
+  let num = getRandomNumber();
+  if (num === 0) 
+  {
+    return "rock";
+  } else if (num === 1) {
+    return "paper";
+  } else if (num === 2) {
+    return "scissors";
+  }
+}
