fef76cf20bc26df80e6109abac106a503e32e9cd
Kenneth Espaldon <kenespaldon@gmail.com>
2025-02-11 16:12:31 -0800

    implement ui functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const humanChoiceDisplay = document.querySelector(".player-choice");
+const computerChoiceDisplay = document.querySelector(".computer-choice");
+const humanScoreDisplay = document.querySelector(".player-score");
+const computerScoreDisplay = document.querySelector(".computer-score");
+const currentRoundDisplay = document.querySelector(".current-round");
+const roundResultDisplay = document.querySelector(".round-results");
+
+const symbols = document.querySelectorAll(".symbol");
+
+const resetBtn = document.querySelector(".reset-btn");
+
 function getRandomNumber() 
 {
   return Math.floor(Math.random() * 3);
@@ -51,39 +62,76 @@ function displayRoundResult(result, humanChoice, computerChoice)
 
   if (result === "human")
   {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    roundResultDisplay.textContent = "Player wins!";
   }
   else if (result === "computer")
   {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    roundResultDisplay.textContent = "Computer wins!";
   }
   else
   {
-    console.log("It was a tie!");
+    roundResultDisplay.textContent = "Tie!";
   }
 }
 
-function displayWinner(humanScore, computerScore)
+function displayChosenSymbols(humanChoice, computerChoice) 
 {
-  if (humanScore === computerScore)
+  humanChoiceDisplay.textContent = humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1);
+  computerChoiceDisplay.textContent = computerChoice.slice(0, 1).toUpperCase() + computerChoice.slice(1);
+}
+
+function displayCurrentRound(round, humanScore, computerScore)
+{
+  if (!checkForWinner(humanScore, computerScore))
   {
-    console.log(`Tie game! ${humanScore} - ${computerScore}`);
-  }
-  else if (humanScore > computerScore) 
-  {
-    console.log(`You win! ${humanScore} - ${computerScore}`);
+    currentRoundDisplay.textContent = `Round ${round}`;
   }
   else
   {
-    console.log(`You lose! ${humanScore} - ${computerScore}`);
+    currentRoundDisplay.textContent = "GAME OVER";
   }
+}
+
+function displayScores(humanScore, computerScore)
+{
+  humanScoreDisplay.textContent = humanScore;
+  computerScoreDisplay.textContent = computerScore;
+}
+
+function checkForWinner(humanScore, computerScore) 
+{
+  if (humanScore === 5 || computerScore === 5)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+function resetUI() 
+{
+  humanChoiceDisplay.textContent = "";
+  humanScoreDisplay.textContent = 0;
+  computerChoiceDisplay.textContent = "";
+  computerScoreDisplay.textContent = 0;
+  currentRoundDisplay.textContent = "Round 1";
+  roundResultDisplay.textContent = "";
+}
+
+function updateUI(result, round, result, humanScore, computerScore, humanChoice, computerChoice)
+{
+  displayRoundResult(result, humanChoice, computerChoice);
+  displayChosenSymbols(humanChoice, computerChoice);
+  displayCurrentRound(round, humanScore, computerScore);
+  displayScores(humanScore, computerScore);
 }
 
 function playGame() 
 {
   let humanScore = 0;
   let computerScore = 0;
-  
+  let round = 1;
+
   function playRound(humanChoice, computerChoice) 
   {
     humanChoice = humanChoice.toLowerCase();
@@ -97,19 +145,58 @@ function playGame()
     {
       computerScore++;
     }
-  
-    console.log("Computer chose " + computerChoice);
-    displayRoundResult(result, humanChoice, computerChoice);
+    
+    updateUI(result, round, result, humanScore, computerScore, humanChoice, computerChoice);
+
+    if (!checkForWinner(humanScore, computerScore))
+    {
+      round++;
+    }
+    else 
+    {
+      symbols.forEach((symbol) => {
+        symbol.removeEventListener("click", chooseSymbol);
+      });
+    }
   }
 
-  for (let round = 1; round <= 5; round++)
+  function chooseSymbol(symbol) 
   {
-    let humanSelection = getHumanChoice();
     let computerSelection = getComputerChoice();
+    let humanSelection = "";
+
+    if (symbol.target.classList.contains("rock-symbol")) 
+    {
+      humanSelection = "rock";
+    }
+    else if (symbol.target.classList.contains("paper-symbol"))
+    {
+      humanSelection = "paper";
+    }
+    else
+    {
+      humanSelection = "scissors";
+    }
+
     playRound(humanSelection, computerSelection);
   }
 
-  displayWinner(humanScore, computerScore);
+  function enableSymbolButtons()
+  {
+    symbols.forEach((symbol) => {
+      symbol.addEventListener("click", chooseSymbol);
+    });  
+  }
+
+  resetBtn.addEventListener("click", () => {
+      resetUI();
+      enableSymbolButtons();
+      round = 1;
+      humanScore = 0;
+      computerScore = 0;
+  });
+
+  enableSymbolButtons();
 }
 
 playGame();
